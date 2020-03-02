758b864f289eec5389683f40bd3ae1e1a4d8b22c
dimo4ka <gldima68@gmail.com>
2020-03-03 02:23:17 +0300

    project v1 Kirill

---
+++ graphic.h
@@ -0,0 +1,39 @@
+#ifndef GRAPHIC_H
+#define GRAPHIC_H
+
+#include <QWidget>
+#include <QColor>
+class Graphic : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Graphic(QWidget *parent = 0);
+
+    QSize minimumSizeHint() const Q_DECL_OVERRIDE;
+    QSize sizeHint() const Q_DECL_OVERRIDE;
+
+    enum Functions{circle, clover,Archimedes,snail};
+
+    void setBackgroundColor(QColor color){ mBackgroundColor = color;}
+    QColor BackgroundColor() const{ return mBackgroundColor;}
+
+    void setFunction(Functions function){mFunction= function; on_function_change();}
+    Functions function () const{return mFunction; }
+protected:
+        void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+
+signals:
+
+
+private:
+       QPointF compute_function(float t);
+       QPointF compute_circle(float t);
+       QPointF compute_clover(float t);
+       QPointF compute_Archimedes(float t);
+        QPointF compute_snail(float t);
+       void on_function_change();
+      QColor mBackgroundColor;
+     Functions mFunction;
+};
+
+#endif // GRAPHIC_H
---
+++ mainwindow.cpp
@@ -0,0 +1,44 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_circle_clicked()
+{
+    this->ui->graphic->setBackgroundColor(Qt::white);
+    this->ui->graphic->setFunction(Graphic::circle);
+    this->ui->graphic->repaint();
+}
+
+void MainWindow::on_clover_clicked()
+{
+    this->ui->graphic->setBackgroundColor(Qt::white);
+    this->ui->graphic->setFunction(Graphic::clover);
+    this->ui->graphic->repaint();
+}
+
+void MainWindow::on_Archimedes_clicked()
+{
+    this->ui->graphic->setBackgroundColor(Qt::white);
+    this->ui->graphic->setFunction(Graphic::Archimedes);
+    this->ui->graphic->repaint();
+}
+
+
+void MainWindow::on_snail_clicked()
+{
+    this->ui->graphic->setBackgroundColor(Qt::white);
+    this->ui->graphic->setFunction(Graphic::snail);
+    this->ui->graphic->repaint();
+}
